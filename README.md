08447a0bdad0eacc4d012b119192fc2e6800d591
jappelgren <justinappelgren@gmail.com>
2021-04-21 14:46:27 -0500

    user can create walls that square cant traverse through, channged icon from blue square to a dog

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a quick little app experimenting with moving a colored cube around a custom grid with the arrow keys on the keyboard.
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ function App() {
   const [widthDepth, setWidthDepth] = useState({ width: 1, depth: 1 });
   const [position, setPosition] = useState([5, 5]);
 
+  const makeWall = (array) => {
+    console.log(array);
+    const prevGrid = [...grid];
+    if (prevGrid[array[0]][array[1]] !== 2) {
+      prevGrid[array[0]][array[1]] = 2;
+    } else {
+      prevGrid[array[0]][array[1]] = 0;
+    }
+    setGrid(prevGrid);
+  };
+
   function gridBuilder(width: number, depth: number): void {
     let gridArr: number[][] = [];
 
@@ -28,10 +39,9 @@ function App() {
     event.preventDefault();
     gridBuilder(widthDepth.width, widthDepth.depth);
     gridRef.current.focus();
-    
   };
 
-  const positionFinder = () => {
+  const positionFinder = (): number[] => {
     for (let i = 0; i < widthDepth?.depth; i++) {
       for (let j = 0; j < widthDepth?.width; j++) {
         if (grid?.[i]?.[j] === 1) {
@@ -43,19 +53,25 @@ function App() {
 
   useEffect(() => {
     setPosition(positionFinder());
-  }, [grid])
-
-  
+  }, [grid]);
 
   document.onkeydown = (event: any): void => {
     event = event || window.event;
-    if (event.key === 'ArrowRight' && position[1] < widthDepth.width - 1) {
+    if (
+      event.key === 'ArrowRight' &&
+      position[1] < widthDepth.width - 1 &&
+      grid[position[0]][position[1] + 1] !== 2
+    ) {
       const prevGrid = [...grid];
       prevGrid[position[0]][position[1]] = 0;
       prevGrid[position[0]][position[1] + 1] = 1;
       setGrid(prevGrid);
       setPosition(positionFinder());
-    } else if (event.key === 'ArrowLeft' && position[1] > 0) {
+    } else if (
+      event.key === 'ArrowLeft' &&
+      position[1] > 0 &&
+      grid[position[0]][position[1] - 1] !== 2
+    ) {
       const prevGrid = [...grid];
       prevGrid[position[0]][position[1]] = 0;
       prevGrid[position[0]][position[1] - 1] = 1;
@@ -63,14 +79,19 @@ function App() {
       setPosition(positionFinder());
     } else if (
       event.key === 'ArrowDown' &&
-      position[0] < widthDepth.depth - 1
+      position[0] < widthDepth.depth - 1 &&
+      grid[position[0] + 1][position[1]] !== 2
     ) {
       const prevGrid = [...grid];
       prevGrid[position[0]][position[1]] = 0;
       prevGrid[position[0] + 1][position[1]] = 1;
       setGrid(prevGrid);
       setPosition(positionFinder());
-    } else if (event.key === 'ArrowUp' && position[0] > 0) {
+    } else if (
+      event.key === 'ArrowUp' &&
+      position[0] > 0 &&
+      grid[position[0] - 1][position[1]] !== 2
+    ) {
       const prevGrid = [...grid];
       prevGrid[position[0]][position[1]] = 0;
       prevGrid[position[0] - 1][position[1]] = 1;
@@ -119,11 +140,22 @@ function App() {
           {grid.map((row, i) => {
             return (
               <section key={i} className="row">
-                {grid[i].map((cell, i) => {
+                {grid[i].map((cell, j) => {
                   return cell === 1 ? (
-                    <div key={i} className="cell blue"></div>
+                    <div
+                      key={j}
+                      className="cell blue"
+                      style={{
+                        backgroundImage: 'url(/images/gabe.jpg)',
+                        backgroundSize: 'cover',
+                      }}
+                    ></div>
                   ) : (
-                    <div key={i} className="cell"></div>
+                    <div
+                      key={j}
+                      onClick={() => makeWall([i, j])}
+                      className={`cell ${cell === 2 ? 'black' : ''}`}
+                    ></div>
                   );
                 })}
               </section>
